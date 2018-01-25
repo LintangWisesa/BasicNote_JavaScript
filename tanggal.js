2583e72c4d6ee88aca0b9118aa742850c833fa13
Lintang Wisesa <lintangwisesa@ymail.com>
2018-01-25 13:21:50 +0700

    Add files via upload

---
+++ tanggal.js
@@ -0,0 +1,8 @@
+
+let a = new Date()
+console.log('')
+console.log(' Hari ini tanggal '+a.getDate()+'-'+(a.getMonth()+1)+'-'+a.getFullYear())
+console.log('')
+console.log(' Besok tanggal '+(a.getDate()+1)+'-'+(a.getMonth()+1)+'-'+a.getFullYear())
+console.log('')
+console.log(' Kemarin tanggal '+(a.getDate()-1)+'-'+(a.getMonth()+1)+'-'+a.getFullYear())
